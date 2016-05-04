4475b476cc8139b7848b2dba71dffa1171b2e02a
Jo Yep <joyep@Jos-Mac-mini.local>
2016-05-04 18:04:50 -0500

    Adding more constants and Logger

--- employee_spec.js
+++ employee_spec.js
@@ -1,11 +1,14 @@
 var frisby = require('./lib/frisby');
 var constants = require('./lib/constants');
+var token = "";
 
+log('=====================================================');
+log('Employee Test Authentication');
 
 frisby.create('Authentication Test')
-    .post(constants.MAIN_URL + constants.EMPLOYEE +'authenticate/', {
-        username : constants.USERNAME,
-        password : constants.PASSWORD
+    .post(constants.MAIN_URL + constants.EMPLOYEE + 'authenticate/', {
+        username: constants.USER_NAME,
+        password: constants.USER_PASSWORD
     }, {
         json: true
     }, {
@@ -19,6 +22,8 @@ frisby.create('Authentication Test')
         token: String,
         user_id: Number
     })
+    .inspectRequest()
+    .inspectHeaders()
     .afterJSON(
         function(res) {
             frisby.globalSetup({
@@ -28,45 +33,79 @@ frisby.create('Authentication Test')
                     }
                 }
             });
-            log('Autenticated username :'+ constants.USERNAME +' and with token : ' + res.token);
+            token = res.token;
+            log('Employee Test Authentication Passed');
+            log('=====================================================');
+            log('Employee List Test');
+            //GET /api/employee/list/ Returns the full employee list
             frisby.create('Employee List Test')
-                .get(constants.MAIN_URL + constants.EMPLOYEE + 'list/')
-                .expectStatus(200)
-                .addHeader('Authorization', 'Token ' + res.token)
+                .get(constants.MAIN_URL + constants.EMPLOYEE + constants.LIST)
+                .addHeader('Authorization', 'Token ' + token)
                 .expectHeaderContains('Content-Type', 'application/json')
+                .expectStatus(200)
                 .expectJSONTypes({
-                count: Number,
-                next: String,
-                previous: function(value) {
-                  if( value === "undefined"){
-                    value=0;
-                  }else if (value === "null"){
-                    value=0;
-                  }
-                  },
-                results: Object
+                    count: Number,
+                    next: String,
+                    previous: function(value) {
+                        if (value === "undefined") {
+                            value = 0;
+                        } else if (value === "null") {
+                            value = 0;
+                        }
+                    },
+                    results: Object
                 })
-                .expectJSONTypes('results.*',{
-                  pk: Number,
-                  username: String,
-                  email: String,
-                  first_name: String,
-                  last_name: String,
-                  level: Number,
-                  avatar: String,
-                  score: Number,
-                  last_month_score: Number,
-                  current_month_score: Number
+                .inspectRequest()
+                .expectJSONTypes('results.*', {
+                    pk: Number,
+                    username: String,
+                    email: String,
+                    first_name: String,
+                    last_name: String,
+                    level: Number,
+                    avatar: String,
+                    total_score: Number,
+                    last_month_score: Number,
+                    last_year_score: Number,
+                    current_month_score: Number,
+                    current_year_score: Number,
+                    last_month_score: Number,
+                    current_month_score: Number
                 })
                 .afterJSON(
-                        function(res) {
-                            log('Employee List Test Passed');
-                })
+                    function(res) {
+                        log('Employee List Test Passed');
+                        log('=====================================================');
+                        log('Employee List Test 2');
+                        //GET /api/employee/list/top/{kind}/{quantity}/ Returns top {quantity} list, {kind} (total_score, level, last_month_score, current_month_score, last_year_score, current_year_score)
+                        frisby.create('Employee List Test 2')
+                            .get(constants.MAIN_URL + constants.EMPLOYEE + constants.LIST + constants.TOP + constants.EMP_TOTAL_SCORE + '/2')
+                            .addHeader('Authorization', 'Token ' + token)
+                            .expectStatus(200)
+                            .expectHeaderContains('Content-Type', 'application/json')
+                            .inspectRequest()
+                            .expectJSONTypes('?', {
+                                "pk": Number,
+                                "username": String,
+                                "first_name": String,
+                                "last_name": String,
+                                "avatar": String,
+                                "value": Number
+                            })
+                            .afterJSON(
+                                function() {
+                                    log('Employee List Test 2 Passed');
+                                    log('=====================================================');
+
+                                })
+                            .toss();
+                    })
                 .toss();
+
         })
     .toss();
 
 
-function log(params){
+function log(params) {
     console.log(params);
 }
