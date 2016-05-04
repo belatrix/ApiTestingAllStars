e4efa191f6a9a501a0d7e93caecb4915a8fe2cb2
Gladys C <gcuzcano@Gladyss-Mac-mini.local>
2016-05-04 15:28:42 -0500

    Adding new test for category and new constants values

--- category_spec.js
+++ category_spec.js
@@ -29,33 +29,79 @@ frisby.create('Authentication Test')
                 }
             });
             log('Autenticated username :'+ constants.USERNAME +' and with token : ' + res.token);
-            frisby.create('Employee List Test')
-                .get(constants.MAIN_URL + constants.EMPLOYEE + 'list/')
+
+            frisby.create('Returns full category list ordered by weight')
+                .get(constants.MAIN_URL + constants.CAT + constants.LIST)
                 .expectStatus(200)
                 .addHeader('Authorization', 'Token ' + res.token)
                 .expectHeaderContains('Content-Type', 'application/json')
                 .expectJSONTypes({
-                    count: Number,
-                    next: String,
-                    previous: null,
-                    results: [{
-                      pk: Number,
-                      username: String,
-                      email: String,
-                      first_name: String,
-                      last_name: String,
-                      level: Number,
-                      avatar: String,
-                      score: Number,
-                      last_month_score: Number,
-                      current_month_score: Number
-                  }]
+                    pk: Number,
+                    name: String,
+                    weight: Number
                 })
+
                 .afterJSON(
                         function(res) {
-                            log('Employee List Test Passed');
+                            log('Category List Test - Passed');
                 })
-                .toss();
+              .toss();
+
+              frisby.create('Returns full subcategory list according to category id')
+              .get(constants.MAIN_URL + constants.CAT + constants.SUBC + constants.LIST)
+              .expectStatus(200)
+              .addHeader('Authorization', 'Token ' + res.token)
+              .expectHeaderContains('Content-Type', 'application/json')
+              .expectJSONTypes({
+                  pk: Number,
+                  name: String
+              })
+
+              .afterJSON(
+                      function(res) {
+                          log('Sub category List Test according category id - Passed');
+              })
+            .toss();
+
+            frisby.create('Returns full subcategory list ordered by name')
+            .get(constants.MAIN_URL + constants.CAT + constants.SUBC + constants.LIST)
+            .expectStatus(200)
+            .addHeader('Authorization', 'Token ' + res.token)
+            .expectHeaderContains('Content-Type', 'application/json')
+            .expectJSONTypes({
+                pk: Number,
+                name: String
+            })
+
+            .afterJSON(
+                    function(res) {
+                        log('Sub category List Test ordered by name - Passed');
+            })
+            .toss();
+
+            frisby.create('Returns subcategory detail category list')
+            .get(constants.MAIN_URL + constants.CAT + constants.SUBC)
+            .expectStatus(200)
+            .addHeader('Authorization', 'Token ' + res.token)
+            .expectHeaderContains('Content-Type', 'application/json')
+            .expectJSONTypes({
+                pk: Number,
+                name: String,
+                category: String
+            })
+
+            .expectJSONTypes('category.*',{
+              id: Number,
+              name: String,
+              weight: Number,
+              comment_required: Boolean
+            })
+            .afterJSON(
+                    function(res) {
+                        log('Sub category List Detail Test - Passed');
+            })
+           .toss();
+
         })
     .toss();
 
