09ba9561774a655910c6f3b5dec3851cfea3bba4
Gladys C <gcuzcano@Gladyss-Mac-mini.local>
2016-05-10 16:43:10 -0500

    Adding url to point local host

--- category_spec.js
+++ category_spec.js
@@ -6,20 +6,22 @@ var constants = require('./lib/constants');
                 .get(constants.MAIN_URL + constants.CAT + constants.LIST)
                 .expectStatus(200)
                 .expectHeaderContains('Content-Type', 'application/json')
-                .expectJSONTypes({
-                    pk: undefined,
-                    name: undefined,
-                    weight: undefined,
-                    comment_required: undefined
+                .expectJSONTypes('?',{
+                    pk: Number,
+                    name: String,
+                    weight: Number,
+                    comment_required: Boolean
                 })
 
+
                 .afterJSON(function(json) {
-                  console.log(json);
+                  console.log(json.pk);
                   for (var i =0 ; i<json.length; i++) {
                       console.log("For " + i);
                       var val = json[i];
                       subcategoryList(val);
                   }
+
 
         //////////////// Subcategory List according de ID
                 function subcategoryList(val){
@@ -28,9 +30,9 @@ var constants = require('./lib/constants');
                     .get(constants.MAIN_URL + constants.CAT + val.pk +'/' + constants.SUBC + constants.LIST)
                     .expectStatus(200)
                     .expectHeaderContains('Content-Type', 'application/json')
-                    .expectJSONTypes({
-                        pk: undefined,
-                        name: undefined
+                    .expectJSONTypes('?',{
+                        pk: Number,
+                        name: String
                     })
 
                     .afterJSON(
@@ -47,9 +49,9 @@ var constants = require('./lib/constants');
                   .get(constants.MAIN_URL + constants.CAT + constants.KEY + constants.LIST)
                   .expectStatus(200)
                   .expectHeaderContains('Content-Type', 'application/json')
-                  .expectJSONTypes({
-                      pk: undefined,
-                      name: undefined
+                  .expectJSONTypes('?',{
+                      pk: Number,
+                      name: String
                   })
 
                   .toss();
@@ -62,38 +64,37 @@ var constants = require('./lib/constants');
             .get(constants.MAIN_URL + constants.CAT + constants.SUBC + constants.LIST)
             .expectStatus(200)
             .expectHeaderContains('Content-Type', 'application/json')
-            .expectJSONTypes({
-                pk: undefined,
-                name: undefined
+            .expectJSONTypes('?',{
+                pk: Number,
+                name: String
             })
 
             .afterJSON(function(json) {
-              console.log(json);
+              console.log(json.pk);
+              console.log("----");
               for (var i =0 ; i<json.length; i++) {
-                  console.log("On the for" + i);
+                  console.log("On the for " + i);
                   var val = json[i];
                   subcategoryDetail(val);
               }
 
+////////////// Subcategory - Category
             function subcategoryDetail(val){
                 console.log("Category: " + val.name + " - id: " +val.pk);
                 frisby.create('Returns full subcategory list according to category id')
                 .get(constants.MAIN_URL + constants.CAT + constants.SUBC + val.pk +'/')
                 .expectStatus(200)
                 .expectHeaderContains('Content-Type', 'application/json')
-                .expectJSONTypes({
-                    pk: undefined,
-                    name: undefined,
-                    category: undefined
-                })
-
-                .expectJSONTypes('category.*',{
-                  id: undefined,
-                  name: undefined,
-                  weight: undefined,
-                  comment_required: undefined
+                .expectJSONTypes('*',{
+                    pk: String,
+                    name: String,
+                    category: {
+                      id: Number,
+                      name: String,
+                      weight: Number,
+                      comment_required: Boolean
+                    }
                 });
-
                 }
               })
               .toss();
