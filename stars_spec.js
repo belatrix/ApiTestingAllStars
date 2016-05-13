ac8dbfca4ae3f026caf4add6d5552cfd482cecef
Luis Barzola <lbarzolao@gmail.com>
2016-05-13 10:26:17 -0500

    Tests added

--- stars_spec.js
+++ stars_spec.js
@@ -36,6 +36,7 @@ frisby.create('Authentication Test')
                 .post(constants.MAIN_URL + constants.STAR + constants.IDSERGIO + constants.GIVESTARS + constants.IDBOBBY + '/' , {
                     "category": "1",
                     "subcategory": "1",
+                    "keyword": "1",
                     "text": "MVP"
                 },  {
                     json: true
@@ -52,7 +53,46 @@ frisby.create('Authentication Test')
                             }
                         });
 
-                    })
+                    frisby.create('Get star')
+                      .get(constants.MAIN_URL + constants.STAR + constants.STAR_ID)
+                      .addHeader('Authorization', 'Token' + token)
+                      .expectStatus(200)
+                      .expectJSONTypes({
+                          pk: Number,
+                          date: String,
+                          text: String,
+                          from_user: Number,
+                          to_user: Number,
+                          category: Number,
+                          subcategory: Number,
+                          keyword: Number
+                      })
+                      .afterJSON(function (json){
+                        console.log("\nStar:");
+                        console.log("ID: " +json.pk+"\nFrom user: "+ json.from_user);
+
+                          frisby.create('Get stars list from employee')
+                            .get(constants.MAIN_URL + constants.STAR + constants.IDBOBBY + '/' + constants.LIST)
+                            .expectStatus(200)
+                            /*.expectJSONTypes({
+                              pk: Number,
+                              date: String,
+                              text: String,
+                              from_user: Number,
+                              to_user: Number,
+                              category: Number,
+                              subcategory: Number,
+                              keyword: Number
+                            })*/
+                            //.afterJSON(function (json){
+                              //for(var i=0; i<json.length; i++){
+                                //console.log(json.pk);
+                              //}
+                            //})
+                            .toss();
+                      })
+                      .toss();
+                })
                 .toss();
         })
     .toss();
